c563501918241a2187a421bbe0094d3cf4665776
alihinnawe <hinnaweali@gmail.com>
2024-02-16 17:29:26 +0100

    push my react portfolio to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import "../src/styles.css";
 
-function App() {
+function Header() {
+  useEffect(() => {
+    let url = window.location.href.split("/");
+    let target = url[url.length - 1].toLowerCase();
+    let element = document.getElementById(target);
+    element && element.scrollIntoView({ behavior: "smooth", block: "start" });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Link
+        to="/"
+        onClick={() => {
+          let resume = document.getElementById("resume");
+          resume &&
+            resume.scrollIntoView({ behavior: "smooth", block: "start" });
+        }}
+      >
+        Resume
+      </Link>
+      <Link
+        to="/about"
+        onClick={() => {
+          let about = document.getElementById("about");
+          about && about.scrollIntoView({ behavior: "smooth", block: "start" });
+        }}
+      >
+        About
+      </Link>
+      <Link
+        to="/contact"
+        onClick={() => {
+          let contact = document.getElementById("contact");
+          contact &&
+            contact.scrollIntoView({ behavior: "smooth", block: "start" });
+        }}
+      >
+        Contact
+      </Link>
+    </>
   );
 }
 
-export default App;
+function Resume() {
+  return (
+    <section id="resume">
+      <h1>Resume Section</h1>
+    </section>
+  );
+}
+
+function About() {
+  return (
+    <section id="about">
+      <h1>About Section</h1>
+      <></>
+    </section>
+  );
+}
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h1>Contact Section</h1>
+    </section>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Resume />
+        <About />
+        <Contact />
+      </BrowserRouter>
+    </div>
+  );
+}
